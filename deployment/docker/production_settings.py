9dcf66e154b4bf09f20671693cb70ee79f086262
Le Tien <tienoho@gmail.com>
2026-01-15 16:18:40 +0700

    feat: Add production settings and mount external pretix.cfg for Docker deployments.

--- deployment/docker/production_settings.py
+++ deployment/docker/production_settings.py
@@ -2,3 +2,5 @@ from pretix.settings import *
 
 LOGGING['handlers']['mail_admins']['include_html'] = True
 STORAGES["staticfiles"]["BACKEND"] = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+ALLOWED_HOSTS = ['*']
