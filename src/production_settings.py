835cf22f0d31ce82b62b6eae5ec305443e592665
Le Tien <tienoho@gmail.com>
2026-01-15 15:51:14 +0700

    Remove local pretix source volume mount from web and worker services in docker-compose.yml.

--- src/production_settings.py
+++
@@ -1,5 +0,0 @@
-from pretix.settings import *
-
-LOGGING['handlers']['mail_admins']['include_html'] = True
-# Disabled: manifest file not available when code is mounted from host
-# STORAGES["staticfiles"]["BACKEND"] = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
