a0a1c077a38b1336004c2481f123585feea373bb
Le Tien <tienoho@gmail.com>
2026-01-15 16:55:43 +0700

    ádasd

--- deployment/docker/production_settings.py
+++ deployment/docker/production_settings.py
@@ -4,3 +4,14 @@ LOGGING['handlers']['mail_admins']['include_html'] = True
 STORAGES["staticfiles"]["BACKEND"] = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 ALLOWED_HOSTS = ['*']
+
+# Allow any host to be treated as a system domain (bypassing checking against SITE_URL)
+class AllHosts:
+    def __contains__(self, item):
+        return True
+
+getLocalHostNames = AllHosts()
+# We can't easily override the imported constant LOCAL_HOST_NAMES from settings directly if it's not re-read.
+# Wait, middlewares import 'from django.conf import settings'. 
+# settings object values come from this file.
+LOCAL_HOST_NAMES = AllHosts()
